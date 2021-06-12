e1fc9e281ec7253aa6eccbd9ea020335538be788
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2021-06-12 17:22:51 -0500

    Documentation of login, navigation, owner, singp, styles and vet

--- Javascript/Navigation.js
+++ Javascript/Navigation.js
@@ -1,11 +1,21 @@
+/**
+ * Function of bootstrap table for owner
+ */
+
 $(document).ready(function () {
     $('#myTableOwner').DataTable();
 });
 
+/**
+ * Function of bootstrap table for owner's table cases and visits
+ */
+
 $(document).ready(function () {
     $('#myTableCaseVisitOwner').DataTable();
 });
-
+/**
+ * Function of bootstrap table for pets
+ */
 $(document).ready(function () {
     $('#myTablePets').DataTable();
 });
